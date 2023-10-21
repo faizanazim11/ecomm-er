bcd82df2a5b480b403bbb69532d2db9d6ffa51a9
shoumikssahu <shoumikssahu@gmail.com>
2023-10-21 20:24:05 +0530

    CSS Setup created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Homepage } from "pages";
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+    </Routes>
+  );
 };
 
 export default App;
